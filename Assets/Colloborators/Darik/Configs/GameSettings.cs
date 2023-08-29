5ffbe6e60eab806c96ed81b50d8b0dd46a21419d
Daighter <arkansiel@naver.com>
2023-08-29 16:30:36 +0900

    feat : 불필요한 선언 네이스펭이스 제거

--- Assets/Colloborators/Darik/Configs/GameSettings.cs
+++ Assets/Colloborators/Darik/Configs/GameSettings.cs
@@ -10,7 +10,7 @@ namespace Darik
         private static void Init()
         {
             // 게임을 시작하기 전 필요한 설정들
-            if (Darik.GameManager.Instance == null)
+            if (GameManager.Instance == null)
             {
                 GameObject gameManager = new GameObject() { name = "GameManager" };
                 gameManager.AddComponent<GameManager>();
