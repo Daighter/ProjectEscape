55e8ae1d8ad4c5044cf742e72daa13642bbd9a89
Daighter <arkansiel@naver.com>
2023-08-31 18:06:52 +0900

    feat : 게임매니저 네임스페이스 삭제

--- Assets/Colloborators/Darik/Scripts/Managers/GameManager.cs
+++ Assets/Colloborators/Darik/Scripts/Managers/GameManager.cs
@@ -2,59 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Darik
+public class GameManager : MonoBehaviour
 {
-    public class GameManager : MonoBehaviour
+    private static GameManager instance;
+    private static Darik.ResourceManager resourceManager;
+    private static Darik.PoolManager poolManager;
+    private static Darik.DataManager dataManager;
+    private static Darik.SceneManager sceneManager;
+    //private static UIManager uiManager;
+
+    public static GameManager Instance { get { return instance; } }
+    public static Darik.ResourceManager Resource { get { return resourceManager; } }
+    public static Darik.DataManager Data { get { return dataManager; } }
+    public static Darik.PoolManager Pool { get { return poolManager; } }
+    public static Darik.SceneManager Scene { get { return sceneManager; } }
+    //public static UIManager UI { get { return uiManager; } }
+
+    private void Awake()            // 유니티용 중복제거
     {
-        private static GameManager instance;
-        private static ResourceManager resourceManager;
-        private static DataManager dataManager;
-        private static PoolManager poolManager;
-        //private static UIManager uiManager;
-
-        public static GameManager Instance { get { return instance; } }
-        public static ResourceManager Resource { get { return resourceManager; } }
-        public static DataManager Data { get { return dataManager; } }
-        public static PoolManager Pool { get { return poolManager; } }
-        //public static UIManager UI { get { return uiManager; } }
-
-        private void Awake()            // 유니티용 중복제거
+        if (instance != null)
         {
-            if (instance != null)
-            {
-                Destroy(this);
-                return;
-            }
-
-            DontDestroyOnLoad(this);
-            instance = this;
-            InitManagers();
+            Destroy(this);
+            return;
         }
 
-        private void OnDestroy()
-        {
-            if (instance == this)
-                instance = null;
-        }
+        DontDestroyOnLoad(this);
+        instance = this;
+        InitManagers();
+    }
 
-        private void InitManagers()
-        {
-            // 게임을 시작하기 전 필요한 설정들
-            GameObject resourceObj = new GameObject() { name = "ResourceManager" };
-            resourceObj.transform.SetParent(transform);
-            resourceManager = resourceObj.AddComponent<ResourceManager>();
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
 
-            GameObject dataObj = new GameObject() { name = "DataManager" };
-            dataObj.transform.SetParent(transform);
-            dataManager = dataObj.AddComponent<DataManager>();
+    private void InitManagers()
+    {
+        // 게임을 시작하기 전 필요한 설정들
+        GameObject resourceObj = new GameObject() { name = "ResourceManager" };
+        resourceObj.transform.SetParent(transform);
+        resourceManager = resourceObj.AddComponent<ResourceManager>();
 
-            GameObject poolObj = new GameObject() { name = "PoolManager" };
-            poolObj.transform.SetParent(transform);
-            poolManager = poolObj.AddComponent<PoolManager>();
-            /*
-            GameObject uiObj = new GameObject() { name = "UIManager" };
-            uiObj.transform.SetParent(transform);
-            uiManager = uiObj.AddComponent<UIManager>();*/
-        }
+        GameObject dataObj = new GameObject() { name = "DataManager" };
+        dataObj.transform.SetParent(transform);
+        dataManager = dataObj.AddComponent<DataManager>();
+
+        GameObject poolObj = new GameObject() { name = "PoolManager" };
+        poolObj.transform.SetParent(transform);
+        poolManager = poolObj.AddComponent<PoolManager>();
+
+        GameObject sceneObj = new GameObject() { name = "SceneManager" };
+        sceneObj.transform.SetParent(transform);
+        sceneManager = sceneObj.AddComponent<SceneManager>();
+
+        /*
+        GameObject uiObj = new GameObject() { name = "UIManager" };
+        uiObj.transform.SetParent(transform);
+        uiManager = uiObj.AddComponent<UIManager>();*/
     }
 }
