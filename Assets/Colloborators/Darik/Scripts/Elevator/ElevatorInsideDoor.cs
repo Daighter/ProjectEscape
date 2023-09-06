05001a900febd117a4da61d23ee7bb7ac7aa608e
Daighter <arkansiel@naver.com>
2023-09-06 19:35:38 +0900

    feat : 엘리베이터 탑승 시 플레이어도 같이 이동

--- Assets/Colloborators/Darik/Scripts/Elevator/ElevatorInsideDoor.cs
+++ Assets/Colloborators/Darik/Scripts/Elevator/ElevatorInsideDoor.cs
@@ -8,17 +8,23 @@ namespace Darik
     {
         [SerializeField] private float moveSpeed = 1f;
 
+        private PlayerTrigger playerTrigger;
         public bool isAlived = true;
         private int curFloor = 0;
 
         public int CurFloor { get { return curFloor; } }
 
+        private void Awake()
+        {
+            playerTrigger = GetComponentInChildren<PlayerTrigger>();
+        }
+
         protected override void Start()
         {
             base.Start();
 
-            curFloor = -2;
-            transform.position = new Vector3(transform.position.x, -6, transform.position.z);
+            curFloor = -1;
+            transform.position = new Vector3(transform.position.x, -3, transform.position.z);
         }
 
         public void Move(int targetFloor)
@@ -26,6 +32,9 @@ namespace Darik
             if (targetFloor == -1)
             {
                 transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                if (playerTrigger.Player != null)
+                    playerTrigger.MoveInElevator(moveSpeed);
+
                 if (transform.position.y >= -3)
                 {
                     transform.position = new Vector3(transform.position.x, -3, transform.position.z);
@@ -36,6 +45,9 @@ namespace Darik
             else if (targetFloor == -2)
             {
                 transform.Translate(Vector3.up * -moveSpeed * Time.deltaTime);
+                if (playerTrigger.Player != null)
+                    playerTrigger.MoveInElevator(-moveSpeed);
+
                 if (transform.position.y <= -6)
                 {
                     transform.position = new Vector3(transform.position.x, -6, transform.position.z);
