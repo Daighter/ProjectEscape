ef0d34bda2797631feea6c77b8a1bf3fde8afc7b
Daighter <arkansiel@naver.com>
2023-09-11 18:12:54 +0900

    feat : 씬 전환 함수 순서 변경

--- Assets/Colloborators/Darik/Scripts/Portal/ClearDoorPortal.cs
+++ Assets/Colloborators/Darik/Scripts/Portal/ClearDoorPortal.cs
@@ -12,7 +12,10 @@ namespace Darik
         private void OnTriggerEnter(Collider other)
         {
             if (playerLayer.Contain(other.gameObject.layer) && sceneName != null)
+            {
+                other.gameObject.GetComponentInChildren<Bae.FadeInOut>().FadeOut();
                 GameManager.Scene.LoadScene(sceneName);
+            }
         }
     }
 }
