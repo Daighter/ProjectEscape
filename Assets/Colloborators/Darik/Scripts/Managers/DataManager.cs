86156c266c46a11ea620408367520d84e9b1a71d
Daighter <arkansiel@naver.com>
2023-09-14 03:51:04 +0900

    feat : 룸 별 펜던트 색 배정 테스트

--- Assets/Colloborators/Darik/Scripts/Managers/DataManager.cs
+++ Assets/Colloborators/Darik/Scripts/Managers/DataManager.cs
@@ -1,11 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Darik
 {
     public class DataManager : MonoBehaviour
     {
+        private void Start()
+        {
+            SetRandomColor();
+        }
+
+        #region PendantColor
+        public enum Color { None, Red, Orange, Yellow, Green, Blue, Purple, Size }
+
+        public Color[] colors;
+
+        public void SetRandomColor()
+        {
+            colors = new Color[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int index = Random.Range(1, (int)Color.Size);
+                if (colors.Contains((Color)index))
+                {
+                    i--;
+                    continue;
+                }
+
+                colors[i] = (Color)index;
+            }
+
+            foreach (Color color in colors)
+            {
+                Debug.Log(color);
+            }
+        }
+
+        public void SetColor(int sceneNum)
+        {
+            Debug.Log(colors[sceneNum]);
+        }
+        #endregion
+
         private string[] dolls = { "", "", "", "", "", "" };
         private int[] times = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
