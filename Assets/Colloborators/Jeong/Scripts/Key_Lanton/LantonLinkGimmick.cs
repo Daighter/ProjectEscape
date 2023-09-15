98798d71ae536e5ec431be75e818dedfb1954f76
정성진 <wjdtjdwls95@naver.com>
2023-09-15 16:57:00 +0900

    feat. Create lanton gimmick clear script in datamanager

--- Assets/Colloborators/Jeong/Scripts/Key_Lanton/LantonLinkGimmick.cs
+++ Assets/Colloborators/Jeong/Scripts/Key_Lanton/LantonLinkGimmick.cs
@@ -13,9 +13,7 @@ namespace Jeong
         private int[] times = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
         public int[] caveTime = new int[3];
-        public bool isCaveClear = false;
-
-
+        
         [SerializeField] LantonPositionController lantonPositionController;
 
         private void Start()
@@ -25,6 +23,11 @@ namespace Jeong
            // lantonPositionController.lantonStand[0] = lantonPositionController.lantonStand[caveTime[0]];
            // lantonPositionController.lantonStand[1] = lantonPositionController.lantonStand[caveTime[1]];
            // lantonPositionController.lantonStand[2] = lantonPositionController.lantonStand[caveTime[2]];
+        }
+
+        private void LantonClaer()
+        {
+            GameManager.Data.isCaveLantonClear = true;
         }
 
         // 랜덤숫자 3개 뽑시(중복없음)
