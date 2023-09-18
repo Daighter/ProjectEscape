919b458d9d141688dc5f12a2e6f5acd514cfa7ed
Daighter <arkansiel@naver.com>
2023-09-18 19:04:27 +0900

    feat : 1층 복도 문 열림상태 유지

--- Assets/Colloborators/Darik/Scripts/1FStairDoorPuzzles/Stair1FDoorPuzzle.cs
+++ Assets/Colloborators/Darik/Scripts/1FStairDoorPuzzles/Stair1FDoorPuzzle.cs
@@ -15,15 +15,27 @@ namespace Darik
         public bool IsCaveRoomClear { get; set; }
         public bool IsColorMatchClear { get; set; }
 
+        private void Start()
+        {
+            if (GameManager.Data.isCorridorDoorOpen)
+                stair1FDoor.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            else
+                stair1FDoor.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
         public void CheckAllClear()
         {
-            if (IsDungeonRoomClear && IsPrisonRoomClear && IsCaveRoomClear && IsColorMatchClear)
+            if (!GameManager.Data.isCorridorDoorOpen)
             {
-                stair1FDoor.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+                if (IsDungeonRoomClear && IsPrisonRoomClear && IsCaveRoomClear && IsColorMatchClear)
+                {
+                    GameManager.Data.isCorridorDoorOpen = true;
+                    stair1FDoor.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
 
-                KeyImage.gameObject.SetActive(false);
-                GameObject facilityKey = GameManager.Resource.Instantiate(GameManager.Resource.Load<GameObject>("Prefabs/Puzzles/Keys/FacilityRoomKey"),
-                    KeyImage.transform.position + new Vector3(0f, 0f, -0.1f), Quaternion.identity);
+                    KeyImage.gameObject.SetActive(false);
+                    GameObject facilityKey = GameManager.Resource.Instantiate(GameManager.Resource.Load<GameObject>("Prefabs/Puzzles/Keys/FacilityRoomKey"),
+                        KeyImage.transform.position + new Vector3(0f, 0f, -0.1f), Quaternion.identity);
+                }
             }
         }
     }
