7e8dad6d08e8f2ae2b12339773c259e9a2ed159b
Daighter <arkansiel@naver.com>
2023-09-18 21:04:36 +0900

    feat : 동굴룸 랜턴 힌트 시계로 표현

--- Assets/Colloborators/Bae/Scripts/UI/TrueEnd.cs
+++ Assets/Colloborators/Bae/Scripts/UI/TrueEnd.cs
@@ -5,48 +5,51 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TrueEnd : MonoBehaviour
+namespace Bae
 {
-    [SerializeField] RectTransform badEndBlack;
-    [SerializeField] string sceneName = null;
-
-    [SerializeField] TMP_Text trueEnd;
-    [SerializeField] Color textColor;
-
-    public void TrueEnding(float trueDuration)
+    public class TrueEnd : MonoBehaviour
     {
-        badEndBlack.gameObject.SetActive(true);
-        badEndBlack.localPosition = new Vector3(0f, 0f, 11f);
-        StartCoroutine(TrueEndRoutine(trueDuration));
-    }
+        [SerializeField] RectTransform badEndBlack;
+        [SerializeField] string sceneName = null;
 
-    IEnumerator TrueEndRoutine(float trueDuration)
-    {
-        float time = 0;
-        while (time < trueDuration)
+        [SerializeField] TMP_Text trueEnd;
+        [SerializeField] Color textColor;
+
+        public void TrueEnding(float trueDuration)
         {
-            Color newColor = textColor;
-            newColor.a = Mathf.Lerp(0, 1, time / trueDuration);
-            trueEnd.color = newColor;
-            time += Time.deltaTime;
-
-            yield return null;
+            badEndBlack.gameObject.SetActive(true);
+            badEndBlack.localPosition = new Vector3(0f, 0f, 11f);
+            StartCoroutine(TrueEndRoutine(trueDuration));
         }
-        Color newColor2 = textColor;
-        newColor2.a = 1;
-        trueEnd.color = newColor2;
 
-        yield return new WaitForSeconds(trueDuration);
-        Quit();
-    }
-    public void Quit()
-    {
+        IEnumerator TrueEndRoutine(float trueDuration)
+        {
+            float time = 0;
+            while (time < trueDuration)
+            {
+                Color newColor = textColor;
+                newColor.a = Mathf.Lerp(0, 1, time / trueDuration);
+                trueEnd.color = newColor;
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+            Color newColor2 = textColor;
+            newColor2.a = 1;
+            trueEnd.color = newColor2;
+
+            yield return new WaitForSeconds(trueDuration);
+            Quit();
+        }
+        public void Quit()
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #elif UNITY_WEBPLAYER
 Application.OpenURL("http://google.com");
 #else
 Application.Quti();
 #endif
+        }
     }
 }
